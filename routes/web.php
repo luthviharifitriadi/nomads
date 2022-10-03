86e2ccd0585c1bd1f162d58a40ed1fa7ac0d1cf8
Luthvi Hari Fitriadi <59356454+luthvihari@users.noreply.github.com>
2022-10-03 19:19:46 +0700

    Install Auth

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,13 @@ Route::get('/checkout/success', 'CheckoutController@success')
 
 Route::prefix('admin')
     -> namespace('Admin')
+    -> middleware(['auth', 'admin'])
     -> group(function() {
         Route::get('/', 'DashboardController@index' )
             ->name('dashboard');
     });
+
+
+Auth::routes();
+
 
