36e9a34b2a6dd319eff0596005232de6a14ed715
Luthvi Hari Fitriadi <59356454+luthvihari@users.noreply.github.com>
2022-10-10 09:33:56 +0700

    index gallery

--- app/TravelPackage.php
+++ app/TravelPackage.php
@@ -19,4 +19,8 @@ class TravelPackage extends Model
     protected $hidden = [
 
     ];
+
+    public function galleries(){
+        return $this->hasMany(Gallery::class. 'travel_packages_id', 'id');
+    }
 }
