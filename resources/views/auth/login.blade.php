e8e8c3eaccd1a58cc359ddad28cdb43be5184afd
Luthvi Hari Fitriadi <59356454+luthvihari@users.noreply.github.com>
2022-11-03 23:46:07 +0700

    merubah tampilan login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,9 +2,6 @@
 
 @section('content')
 
-
-
-
     <main class="login-container">
         <div class="container">
             <div class="row page-login d-flex align-items-center">
@@ -18,65 +15,52 @@
                         <div class="card-body">
                             <div class="text-center">
                                 <img src="/frontend/images/logo.png" alt="" class="w-50 mb-4">
-                            </div>
-                      
+                            </div>                      
                    
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Email address</label>
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Password</label>
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                                <button
+                                    type="sumbit"
+                                    class="btn btn-login btn-block"
+                                >
+                                Sign-in 
+                                </button>
+                                @if (Route::has('password.request'))
+                                <p class="text-center mt-4">
+                                    <a  href="{{ route('password.request') }}">
+                                        Saya lupa password
+                                    </a>
+                                </p>
+                                @endif  
+                            </form>
                         </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                       
-                              <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                        </div>
-                        <div class="form-group form-check">
-                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                            <label class="form-check-label" for="remember">
-                                {{ __('Remember Me') }}
-                            </label>
-
-                        </div>
-
-                        <button
-                            type="sumbit"
-                            class="btn btn-login btn-block"
-                        >
-                        Sign-in 
-                        </button>
-
-                        @if (Route::has('password.request'))
-                         <p class="text-center mt-4">
-                            <a  href="{{ route('password.request') }}">
-                                Saya lupa password
-                            </a>
-                         </p>
-                        @endif
-
-                        
-
-                    </form>
-                     </div>
-                       </div>
+                    </div>
                 </div>
             </div>
         </div>
-      
     </main>
-
-
 @endsection
